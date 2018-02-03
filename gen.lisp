a229129c7e5511ba8bb4933ee8fae1742f2bd00e
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-02-03 08:35:11 +0100

    designated initializer doesnt work:
    (let ((creq :type "struct drm_mode_create_dumb"
    					       :init (list .width=fb->width
    							   .height=fb->height
    							   .bpp=fb->bpp)))
    				     (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_CREATE_DUMB &creq)))
    				     )

--- gen.lisp
+++ gen.lisp
@@ -52,49 +52,36 @@ is replaced with replacement."
 (defmacro with-open-fstream ((f fn &key (dir "/dev/shm")) &body body)
   `(let ((,f :type "std::ofstream" :ctor (comma-list (string ,(format nil "~a/~a" dir fn)))))
      ,@body))
+
 (progn
   (let* ((code `(with-compilation-unit
 		    (with-compilation-unit
 			(raw "//! \\file main.cpp Draw to screen using linux direct rendering manager"))
-					
-		  (include <cstdint>)
-		  (include <cassert>)
-		  (include <sys/types.h>)
-		  (include <sys/stat.h>)
-		  (include <fcntl.h>)
 
-		  (include <sys/ioctl.h>)
-		  
+		  (include <iostream>)
+		  (include <cassert>)
+		  (include <cstdlib>)
+		  (include <cstring>)
+		  (include <sys/mman.h>)
+		  (include <unistd.h>)
+
 		  (raw " ")
-		  (include <drm/drm.h>)
-		  (raw "// drm/drm.h needs to be patched for C++ to compile, one of the datastructures has an element called virtual")
-		  (include <drm/drm_mode.h>)
-		  (include <fstream>)
-		  (include <string>)
-		  (include <vector>)
-		  (include <cstring>) ;; memset
-		  (include "cxxopts.hpp")
-		  (include <iomanip>) ;; setw
-		  (include <array>)
-		  (raw " ")
-		  
-		  (raw " ")
-		  (raw "//! \\mainpage Draw using direct rendering manager")
-		  (raw "//! \\section Introduction")
-		  (raw "//! This repository contains a minimal program to draw to a linux screen. See http://betteros.org/tut/graphics1.php for a tutorial this code is based on.")
+		  (include <xf86drm.h>)
+		  (include <xf86drmMode.h>)
+
+		  (raw "//! This repository contains a minimal program to draw to a linux screen.")
 		  (raw "//! \\section Dependencies ")
 		  (raw "//! - Linux kernel with DRM driver")
-		  (raw " ")
-		  (raw "//! - cxxopts for commandline argument parsing.")
+		  (raw "//! - libdrm")
 		  (raw " ")
 		  (raw "//! - sbcl to generate c++ code")
-		  (raw "//! - cmake to configure for build")
+		  ;(raw "//! - cmake to configure for build")
 		  (raw "//! - g++ to compile c++ code")
 		  (raw " ")
-		  (raw "//! - For the documentation (optional for use):")
+		  (raw "//! - For the documentation (optional):")
 		  (raw "//!   + doxygen")
 		  (raw " ")
-		  (raw "//! - The C++ code can be opened as a cmake project in kdevelop, clion or qtcreator (order of my preference).")
+		  
 		  (raw " ")
 		  (raw "//! \\section References ")
 		  ,@(loop for i from 1 and e in '("http://betteros.org/tut/graphics1.php")
@@ -103,98 +90,59 @@ is replaced with replacement."
 		  
 		  
 		  ,@(dox :brief "main function"
-			 :usage "parse command line parameters and draw to screen"
+			 :usage "draw to screen"
 			 :params '((argc "input number of command line arguments")
 				   (argv "input"))
 			 :return "Integer")
 		  		  
 		  (function (main ((argc :type int)
 				   (argv :type char**)) int)
-
+			    (if (== 0 (funcall drmAvailable))
+				(macroexpand (er "drm not available")))
+			    
+			    (let ((fd :type int :init (funcall drmOpen (string "i915") nullptr)))
+			      (if (< fd 0)
+				  (macroexpand (er "drmOpen error: " fd)))
+			      (let ((r :init (funcall drmSetClientCap fd DRM_CLIENT_CAP_UNIVERSAL_PLANES 1)))
+				(if (!= 0 r)
+				    (macroexpand (er "drmSetClientCap error: " r))))
+			      (let ((res :init  (funcall drmModeGetResources fd)))
+				(funcall assert res)
+				(let ((c :type drmModeConnectorPtr :init nullptr))
+				 (dotimes (i res->count_connectors)
+				   (setf c  (funcall drmModeGetConnector fd (aref res->connectors i)))
+				   (funcall assert c)
+				   (if (== DRM_MODE_CONNECTED
+					   c->connection)
+				       (break))
+				   (funcall drmFree c))
+				 (let ((enc :init (funcall drmModeGetEncoder fd c->encoder_id))
+				       (crtc :init (funcall drmModeGetCrtc fd enc->crtc_id))
+				       (fb :init (funcall drmModeGetFB fd crtc->buffer_id))
+				       (plane_res :init (funcall drmModeGetPlaneResources fd))
+				       (plane :type drmModePlanePtr :init nullptr))
+				   (statements
+				     ,@(loop for e in '(enc crtc fb plane_res) collect
+					    `(funcall assert ,e)))
+				   (dotimes (i plane_res->count_planes)
+				     (setf plane (funcall drmModeGetPlane fd (aref plane_res->planes i)))
+				     (funcall assert plane)
+				     (if (== plane->fb_id fb->fb_id)
+					 (break))
+				     (funcall drmFree plane))
+				   (let ((has_dumb :type uint64_t :init 0))
+				     (funcall assert (! (funcall drmGetCap fd DRM_CAP_DUMB_BUFFER &has_dumb)))
+				     (funcall assert has_dumb))
+				   (let ((creq :type "struct drm_mode_create_dumb"
+					       :init (list .width=fb->width
+							   .height=fb->height
+							   .bpp=fb->bpp)))
+				     (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_CREATE_DUMB &creq)))
+				     )))))
 			    
 			    
-			    
-			    (with-compilation-unit 
-				(raw "try")
-			      (let ((options :type "cxxopts::Options" :ctor (comma-list (string "drm_draw") (string "draw to drm device"))))
-				(with-compilation-unit
-				    (raw "options.add_options()")
-				  (raw "(\"h,help\", \"Print help\")")
-				  (raw "(\"d,device\",\"device file\",cxxopts::value<std::string>()->default_value(\"/dev/dri/card0\"))")
-				  (raw "(\"r,rate\",\"frame rate (Hz,double)\",cxxopts::value<double>()->default_value(\"60.0\"));")
-				  )
-				(funcall options.parse argc argv)
-				
-				(if (funcall options.count (string "help"))
-				    (statements
-				     (macroexpand (e (funcall options.help)))
-				     (funcall exit 0)))
-				(macroexpand (e "requested frame rate = "
-						(funcall "options[\"rate\"].as<double>")
-						" Hz device="
-						(funcall "options[\"device\"].as<std::string>")
-						))
-				(let ((dri_fd :ctor (funcall open "options[\"device\"].as<std::string>().c_str()" O_RDWR)))
-				  (macroexpand (checked-ioctl dri_fd DRM_IOCTL_SET_MASTER 0))
-				  (let ((res{} :type drm_mode_card_res))
-				    (funcall memset &res 0 (funcall sizeof res)) ;; fixme is this required?
-				    (macroexpand (checked-ioctl dri_fd DRM_IOCTL_MODE_GETRESOURCES &res))
-				    (statements ,@(loop for i in '(count_fbs count_crtcs count_connectors count_encoders)
-					  appending
-					    `((macroexpand (e ,(format nil "~a = " i)
-							      (slot-value res ,i)))
-					      (funcall assert (< (slot-value res ,i) 10)))))
-				    (statements ,@(loop for i in '(min_width max_width min_height max_height)
-					  appending
-					    `((macroexpand (e ,(format nil "~a = " i)
-							      (slot-value res ,i)))))))
-				  (let ((resources{} :type drm_mode_card_res))
-				    ,@(loop for i in '(fb crtc connector encoder) appending
-					   `((decl ((,(format nil "~a_array{}" i) :type "std::array<uint64_t,10>")))
-					     (setf (slot-value resources ,(format nil "~a_id_ptr" i))
-						   (funcall reinterpret_cast<uint64_t>
-							    ,(format nil "~a_array.data()" i)))))
-				    (macroexpand (checked-ioctl dri_fd DRM_IOCTL_MODE_GETRESOURCES &resources))
-				    (dotimes (i resources.count_connectors)
-				      (let ((connector{} :type drm_mode_get_connector))
-					(funcall memset &connector 0 (funcall sizeof connector)) ;; fixme is this required?
-					(setf connector.connector_id (funcall connector_array.at i))
-					(macroexpand (checked-ioctl dri_fd DRM_IOCTL_MODE_GETCONNECTOR &connector))
-					(if (\|||\|
-					       (\|||\|
-						  (< connector.count_encoders 1)
-						  (< connector.count_modes 1))
-					       (\|||\|
-						  (! connector.encoder_id)
-						  (! connector.connection)))
-					    (statements
-					     (raw "continue;")))
-					(statements ,@(loop for i in '(count_modes count_props count_encoders)
-							 appending
-							   `((macroexpand (e ,(format nil "~a = " i)
-									     (slot-value connector ,i)))
-							     (funcall assert (< (slot-value connector ,i) 20)))))
-					(setf connector.connector_id (funcall connector_array.at i))
-					,@(loop for i in '(modes) appending
-					       `((decl ((,(format nil "~a_array{}" i) :type "std::array<struct drm_mode_modeinfo,20>")))
-						 (setf (slot-value connector ,(format nil "~a_ptr" i))
-						       (funcall reinterpret_cast<uint64_t>
-								,(format nil "~a_array.data()" i)))))
-					,@(loop for i in '(props prop_values encoders) appending
-					       `((decl ((,(format nil "~a_array{}" i) :type "std::array<uint64_t,20>")))
-						 (setf (slot-value connector ,(format nil "~a_ptr" i))
-						       (funcall reinterpret_cast<uint64_t>
-								,(format nil "~a_array.data()" i)))))
-					(macroexpand (checked-ioctl dri_fd DRM_IOCTL_MODE_GETCONNECTOR &connector))
-					
-					)))))
-			      
-			      (raw "catch (const cxxopts::OptionException& e)")
-			      (let ()
-				(macroexpand (e "error parsing options: " (funcall e.what)))
-				(funcall exit 1)))
 			    
 			    (return 0)))))
-    (write-source "stage/cl-gen-graphics-drm/source/main" "cpp" code)))
+    (write-source "stage/cl-gen-drmgfx/source/main" "cpp" code)))
 
 
