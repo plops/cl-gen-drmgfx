18e1b20df190cf407ce9ecd0990c845f05874f1a
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-02-03 17:42:18 +0100

    flatten the code to one let

--- gen.lisp
+++ gen.lisp
@@ -75,7 +75,7 @@ is replaced with replacement."
 		  (raw "//! - libdrm")
 		  (raw " ")
 		  (raw "//! - sbcl to generate c++ code")
-		  ;(raw "//! - cmake to configure for build")
+					;(raw "//! - cmake to configure for build")
 		  (raw "//! - g++ to compile c++ code")
 		  (raw " ")
 		  (raw "//! - For the documentation (optional):")
@@ -120,69 +120,81 @@ is replaced with replacement."
 							c->connection)
 						    (break))
 						(funcall drmFree c))
-					      (raw "c")))))
+					      (raw "c"))))
+				  (enc :init (funcall drmModeGetEncoder fd c->encoder_id))
+				  (crtc :init (funcall drmModeGetCrtc fd enc->crtc_id))
+				  (fb :init (funcall drmModeGetFB fd crtc->buffer_id))
+				  (plane_res :init (funcall drmModeGetPlaneResources fd))
+				  (plane  :init (paren-list
+						 (let ((p :type drmModePlanePtr :init nullptr))
+						   (statements
+						    ,@(loop for e in '(enc crtc fb plane_res) collect
+							   `(funcall assert ,e)))
+						   (dotimes (i plane_res->count_planes)
+						     (setf p (funcall drmModeGetPlane fd (aref plane_res->planes i)))
+						     (funcall assert p)
+						     (if (== p->fb_id fb->fb_id)
+							 (break))
+						     (funcall drmFree p))
+						   (raw "p"))))
+				  (has_dumb :init (paren-list
+						   (let ((has_dumb :type uint64_t :init 0))
+						     (funcall assert (! (funcall drmGetCap fd DRM_CAP_DUMB_BUFFER &has_dumb)))
+						     (funcall assert has_dumb)
+						     (raw "has_dumb"))))
+				  (creq :init
+				    (paren-list
+				     (let ((creq :type "struct drm_mode_create_dumb"))
+				       (funcall memset &creq 0 (funcall sizeof creq))
+				       (setf creq.width fb->width
+					     creq.height fb->height
+					     creq.bpp fb->bpp)
+				       (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_CREATE_DUMB &creq)))
+				       (macroexpand (e "width=" creq.width " height=" creq.height))
+				       (raw "creq"))))
+				  (my_fb :init (paren-list
+						(let ((my_fb :type uint32_t :init 0))
+						    (funcall assert
+							     (! (funcall drmModeAddFB fd creq.width creq.height 24
+									 creq.bpp creq.pitch creq.handle &my_fb)))
+						    (raw
+						     "my_fb"))))
+				  (mreq :init (paren-list
+					       (let ((mreq :type "struct drm_mode_map_dumb"))
+						 (funcall memset &mreq 0 (funcall sizeof mreq))
+						 (setf mreq.handle creq.handle)
+						 (funcall assert
+							  (! (funcall drmIoctl fd DRM_IOCTL_MODE_MAP_DUMB &mreq)))
+						 (raw "mreq"))))
+				  (map :init (paren-list
+					      (let ((map :type uint32_t*
+							 :init (funcall static_cast<uint32_t*>
+									(funcall mmap 0 creq.size
+											(\| PROT_READ PROT_WRITE)
+											MAP_SHARED fd mreq.offset))))
+						  (funcall assert (!= MAP_FAILED map))
+						  (funcall memset map 0 creq.size)
+						  (raw "map")))))     
 			      
-			      (let ((enc :init (funcall drmModeGetEncoder fd c->encoder_id))
-				    (crtc :init (funcall drmModeGetCrtc fd enc->crtc_id))
-				    (fb :init (funcall drmModeGetFB fd crtc->buffer_id))
-				    (plane_res :init (funcall drmModeGetPlaneResources fd))
-				    (plane  :init (paren-list
-						   (let ((p :type drmModePlanePtr :init nullptr))
-						     (dotimes (i plane_res->count_planes)
-						       (setf p (funcall drmModeGetPlane fd (aref plane_res->planes i)))
-						       (funcall assert p)
-						       (if (== p->fb_id fb->fb_id)
-							   (break))
-						       (funcall drmFree p))
-						     (raw "p")))))
-				(statements
-				 ,@(loop for e in '(enc crtc fb plane_res) collect
-					`(funcall assert ,e)))
-				
-				(let ((has_dumb :type uint64_t :init 0))
-				  (funcall assert (! (funcall drmGetCap fd DRM_CAP_DUMB_BUFFER &has_dumb)))
-				  (funcall assert has_dumb))
-				(let ((creq :type "struct drm_mode_create_dumb"))
-				  (funcall memset &creq 0 (funcall sizeof creq))
-				  (setf creq.width fb->width
-					creq.height fb->height
-					creq.bpp fb->bpp)
-				  (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_CREATE_DUMB &creq)))
-				  (macroexpand (e "width=" creq.width " height=" creq.height))
-				  (let ((my_fb :type uint32_t :init 0))
-				    (funcall assert
-					     (! (funcall drmModeAddFB fd creq.width creq.height 24
-							 creq.bpp creq.pitch creq.handle &my_fb)))
-				    (let ((mreq :type "struct drm_mode_map_dumb"))
-				      (funcall memset &mreq 0 (funcall sizeof mreq))
-				      (setf mreq.handle creq.handle)
-				      (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_MAP_DUMB &mreq)))
-				      (let ((map :type uint32_t*
-						 :init (funcall static_cast<uint32_t*> (funcall mmap 0 creq.size
-												(\| PROT_READ PROT_WRITE)
-												MAP_SHARED fd mreq.offset))))
-					(funcall assert (!= MAP_FAILED map))
-					(funcall memset map 0 creq.size)
-
-					(dotimes (i 256)
-					  (dotimes (j 256)
-					    (setf (aref map (+ j (* i (>> creq.pitch 2))))
-						  (hex #x12345678))))
-					(funcall assert (! (funcall drmModeSetCrtc fd crtc->crtc_id my_fb
-								    0 0 &c->connector_id 1 &crtc->mode)))
-					(funcall sleep 10)
-					(funcall assert (! (funcall drmModeSetCrtc fd crtc->crtc_id fb->fb_id
-								    0 0 &c->connector_id 1 &crtc->mode)))
-					(funcall assert (! (funcall drmModeRmFB fd my_fb)))
-					(let ((dreq :type "struct drm_mode_destroy_dumb"))
-					  (funcall memset &dreq 0 (funcall sizeof dreq))
-					  (setf dreq.handle creq.handle)
-					  (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_DESTROY_DUMB &dreq))))
-					(statements
-					 ,@(loop for e in '(plane plane_res fb crtc enc c res) collect
-						`(funcall drmFree ,e)))
-					(funcall drmClose fd))))))
-)
+			      (dotimes (i 256)
+				(dotimes (j 256)
+				  (setf (aref map (+ j (* i (>> creq.pitch 2))))
+					(hex #x12345678))))
+			      (funcall assert (! (funcall drmModeSetCrtc fd crtc->crtc_id my_fb
+							  0 0 &c->connector_id 1 &crtc->mode)))
+			      (funcall sleep 10)
+			      (funcall assert (! (funcall drmModeSetCrtc fd crtc->crtc_id fb->fb_id
+							  0 0 &c->connector_id 1 &crtc->mode)))
+			      (funcall assert (! (funcall drmModeRmFB fd my_fb)))
+			      (let ((dreq :type "struct drm_mode_destroy_dumb"))
+				(funcall memset &dreq 0 (funcall sizeof dreq))
+				(setf dreq.handle creq.handle)
+				(funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_DESTROY_DUMB &dreq))))
+			      (statements
+			       ,@(loop for e in '(plane plane_res fb crtc enc c res) collect
+				      `(funcall drmFree ,e)))
+			      (funcall drmClose fd)
+			      )
 			    
 			    
 			    
--- source/main.cpp
+++ source/main.cpp
@@ -36,113 +36,119 @@ int main(int argc, char **argv) {
     (std::cerr << "drm not available" << std::endl);
   }
   {
-    int fd = drmOpen("i915", nullptr);
-    if ((fd < 0)) {
-      (std::cerr << "drmOpen error: " << fd << std::endl);
-    }
-    {
-      auto r = drmSetClientCap(fd, DRM_CLIENT_CAP_UNIVERSAL_PLANES, 1);
-      if ((0 != r)) {
-        (std::cerr << "drmSetClientCap error: " << r << std::endl);
+    auto fd = ({
+      auto fd = drmOpen("i915", nullptr);
+      if ((fd < 0)) {
+        (std::cerr << "drmOpen error: " << fd << std::endl);
       }
-    }
-    {
-      auto res = drmModeGetResources(fd);
-      assert(res);
       {
-        drmModeConnectorPtr c = nullptr;
-        for (unsigned int i = 0;
-             (i < static_cast<unsigned int>(res->count_connectors)); i += 1) {
-          c = drmModeGetConnector(fd, res->connectors[i]);
-          assert(c);
-          if ((DRM_MODE_CONNECTED == c->connection)) {
-            break;
-          }
-          drmFree(c);
-        }
-        {
-          auto enc = drmModeGetEncoder(fd, c->encoder_id);
-          auto crtc = drmModeGetCrtc(fd, enc->crtc_id);
-          auto fb = drmModeGetFB(fd, crtc->buffer_id);
-          auto plane_res = drmModeGetPlaneResources(fd);
-          auto plane = ({
-            drmModePlanePtr p = nullptr;
-            for (unsigned int i = 0; (i < plane_res->count_planes); i += 1) {
-              p = drmModeGetPlane(fd, plane_res->planes[i]);
-              assert(p);
-              if ((p->fb_id == fb->fb_id)) {
-                break;
-              }
-              drmFree(p);
-            }
-            p;
-          });
-          assert(enc);
-          assert(crtc);
-          assert(fb);
-          assert(plane_res);
-          {
-            uint64_t has_dumb = 0;
-            assert((!(drmGetCap(fd, DRM_CAP_DUMB_BUFFER, &has_dumb))));
-            assert(has_dumb);
-          }
-          {
-            struct drm_mode_create_dumb creq;
-            memset(&creq, 0, sizeof(creq));
-            creq.width = fb->width;
-            creq.height = fb->height;
-            creq.bpp = fb->bpp;
-            assert((!(drmIoctl(fd, DRM_IOCTL_MODE_CREATE_DUMB, &creq))));
-            (std::cout << "width=" << creq.width << " height=" << creq.height
-                       << std::endl);
-            {
-              uint32_t my_fb = 0;
-              assert((!(drmModeAddFB(fd, creq.width, creq.height, 24, creq.bpp,
-                                     creq.pitch, creq.handle, &my_fb))));
-              {
-                struct drm_mode_map_dumb mreq;
-                memset(&mreq, 0, sizeof(mreq));
-                mreq.handle = creq.handle;
-                assert((!(drmIoctl(fd, DRM_IOCTL_MODE_MAP_DUMB, &mreq))));
-                {
-                  uint32_t *map = static_cast<uint32_t *>(
-                      mmap(0, creq.size, (PROT_READ | PROT_WRITE), MAP_SHARED,
-                           fd, mreq.offset));
-                  assert((MAP_FAILED != map));
-                  memset(map, 0, creq.size);
-                  for (unsigned int i = 0; (i < 256); i += 1) {
-                    for (unsigned int j = 0; (j < 256); j += 1) {
-                      map[(j + (i * (creq.pitch >> 2)))] = 0x12345678;
-                    }
-                  }
-                  assert((!(drmModeSetCrtc(fd, crtc->crtc_id, my_fb, 0, 0,
-                                           &c->connector_id, 1, &crtc->mode))));
-                  sleep(10);
-                  assert((!(drmModeSetCrtc(fd, crtc->crtc_id, fb->fb_id, 0, 0,
-                                           &c->connector_id, 1, &crtc->mode))));
-                  assert((!(drmModeRmFB(fd, my_fb))));
-                  {
-                    struct drm_mode_destroy_dumb dreq;
-                    memset(&dreq, 0, sizeof(dreq));
-                    dreq.handle = creq.handle;
-                    assert(
-                        (!(drmIoctl(fd, DRM_IOCTL_MODE_DESTROY_DUMB, &dreq))));
-                  }
-                  drmFree(plane);
-                  drmFree(plane_res);
-                  drmFree(fb);
-                  drmFree(crtc);
-                  drmFree(enc);
-                  drmFree(c);
-                  drmFree(res);
-                  drmClose(fd);
-                }
-              }
-            }
-          }
+        auto rr = drmSetClientCap(fd, DRM_CLIENT_CAP_UNIVERSAL_PLANES, 1);
+        if ((0 != rr)) {
+          (std::cerr << "drmSetClientCap error: " << rr << std::endl);
         }
       }
+      fd;
+    });
+    auto res = ({
+      auto r = drmModeGetResources(fd);
+      assert(r);
+      r;
+    });
+    drmModeConnectorPtr c = ({
+      drmModeConnectorPtr c = nullptr;
+      for (unsigned int i = 0;
+           (i < static_cast<unsigned int>(res->count_connectors)); i += 1) {
+        c = drmModeGetConnector(fd, res->connectors[i]);
+        assert(c);
+        if ((DRM_MODE_CONNECTED == c->connection)) {
+          break;
+        }
+        drmFree(c);
+      }
+      c;
+    });
+    auto enc = drmModeGetEncoder(fd, c->encoder_id);
+    auto crtc = drmModeGetCrtc(fd, enc->crtc_id);
+    auto fb = drmModeGetFB(fd, crtc->buffer_id);
+    auto plane_res = drmModeGetPlaneResources(fd);
+    auto plane = ({
+      drmModePlanePtr p = nullptr;
+      assert(enc);
+      assert(crtc);
+      assert(fb);
+      assert(plane_res);
+      for (unsigned int i = 0; (i < plane_res->count_planes); i += 1) {
+        p = drmModeGetPlane(fd, plane_res->planes[i]);
+        assert(p);
+        if ((p->fb_id == fb->fb_id)) {
+          break;
+        }
+        drmFree(p);
+      }
+      p;
+    });
+    auto has_dumb = ({
+      uint64_t has_dumb = 0;
+      assert((!(drmGetCap(fd, DRM_CAP_DUMB_BUFFER, &has_dumb))));
+      assert(has_dumb);
+      has_dumb;
+    });
+    auto creq = ({
+      struct drm_mode_create_dumb creq;
+      memset(&creq, 0, sizeof(creq));
+      creq.width = fb->width;
+      creq.height = fb->height;
+      creq.bpp = fb->bpp;
+      assert((!(drmIoctl(fd, DRM_IOCTL_MODE_CREATE_DUMB, &creq))));
+      (std::cout << "width=" << creq.width << " height=" << creq.height
+                 << std::endl);
+      creq;
+    });
+    auto my_fb = ({
+      uint32_t my_fb = 0;
+      assert((!(drmModeAddFB(fd, creq.width, creq.height, 24, creq.bpp,
+                             creq.pitch, creq.handle, &my_fb))));
+      my_fb;
+    });
+    auto mreq = ({
+      struct drm_mode_map_dumb mreq;
+      memset(&mreq, 0, sizeof(mreq));
+      mreq.handle = creq.handle;
+      assert((!(drmIoctl(fd, DRM_IOCTL_MODE_MAP_DUMB, &mreq))));
+      mreq;
+    });
+    auto map = ({
+      uint32_t *map = static_cast<uint32_t *>(mmap(
+          0, creq.size, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, mreq.offset));
+      assert((MAP_FAILED != map));
+      memset(map, 0, creq.size);
+      map;
+    });
+    for (unsigned int i = 0; (i < 256); i += 1) {
+      for (unsigned int j = 0; (j < 256); j += 1) {
+        map[(j + (i * (creq.pitch >> 2)))] = 0x12345678;
+      }
     }
+    assert((!(drmModeSetCrtc(fd, crtc->crtc_id, my_fb, 0, 0, &c->connector_id,
+                             1, &crtc->mode))));
+    sleep(10);
+    assert((!(drmModeSetCrtc(fd, crtc->crtc_id, fb->fb_id, 0, 0,
+                             &c->connector_id, 1, &crtc->mode))));
+    assert((!(drmModeRmFB(fd, my_fb))));
+    {
+      struct drm_mode_destroy_dumb dreq;
+      memset(&dreq, 0, sizeof(dreq));
+      dreq.handle = creq.handle;
+      assert((!(drmIoctl(fd, DRM_IOCTL_MODE_DESTROY_DUMB, &dreq))));
+    }
+    drmFree(plane);
+    drmFree(plane_res);
+    drmFree(fb);
+    drmFree(crtc);
+    drmFree(enc);
+    drmFree(c);
+    drmFree(res);
+    drmClose(fd);
   }
   return 0;
 }
