10549a530935d9ea7df83227797af3a8daaeeae2
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-02-03 17:19:33 +0100

    simplify ctl flow

--- gen.lisp
+++ gen.lisp
@@ -100,82 +100,89 @@ is replaced with replacement."
 			    (if (== 0 (funcall drmAvailable))
 				(macroexpand (er "drm not available")))
 			    
-			    (let ((fd :type int :init (funcall drmOpen (string "i915") nullptr)))
-			      (if (< fd 0)
-				  (macroexpand (er "drmOpen error: " fd)))
-			      (let ((r :init (funcall drmSetClientCap fd DRM_CLIENT_CAP_UNIVERSAL_PLANES 1)))
-				(if (!= 0 r)
-				    (macroexpand (er "drmSetClientCap error: " r))))
-			      (let ((res :init  (funcall drmModeGetResources fd)))
-				(funcall assert res)
-				(let ((c :type drmModeConnectorPtr :init nullptr))
-				 (dotimes (i (funcall "static_cast<unsigned int>" res->count_connectors))
-				   (setf c  (funcall drmModeGetConnector fd (aref res->connectors i)))
-				   (funcall assert c)
-				   (if (== DRM_MODE_CONNECTED
-					   c->connection)
-				       (break))
-				   (funcall drmFree c))
-				 (let ((enc :init (funcall drmModeGetEncoder fd c->encoder_id))
-				       (crtc :init (funcall drmModeGetCrtc fd enc->crtc_id))
-				       (fb :init (funcall drmModeGetFB fd crtc->buffer_id))
-				       (plane_res :init (funcall drmModeGetPlaneResources fd))
-				       (plane  :init (paren-list
-				    (let ((p :type drmModePlanePtr :init nullptr))
-				     (dotimes (i plane_res->count_planes)
-				       (setf p (funcall drmModeGetPlane fd (aref plane_res->planes i)))
-				       (funcall assert p)
-				       (if (== p->fb_id fb->fb_id)
-					   (break))
-				       (funcall drmFree p))
-				     (raw "p")))))
-				   (statements
-				     ,@(loop for e in '(enc crtc fb plane_res) collect
-					    `(funcall assert ,e)))
-				   
-				   (let ((has_dumb :type uint64_t :init 0))
-				     (funcall assert (! (funcall drmGetCap fd DRM_CAP_DUMB_BUFFER &has_dumb)))
-				     (funcall assert has_dumb))
-				   (let ((creq :type "struct drm_mode_create_dumb"))
-				     (funcall memset &creq 0 (funcall sizeof creq))
-				     (setf creq.width fb->width
-					   creq.height fb->height
-					   creq.bpp fb->bpp)
-				     (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_CREATE_DUMB &creq)))
-				     (macroexpand (e "width=" creq.width " height=" creq.height))
-				     (let ((my_fb :type uint32_t :init 0))
-				       (funcall assert
-						(! (funcall drmModeAddFB fd creq.width creq.height 24
-							    creq.bpp creq.pitch creq.handle &my_fb)))
-				       (let ((mreq :type "struct drm_mode_map_dumb"))
-					 (funcall memset &mreq 0 (funcall sizeof mreq))
-					 (setf mreq.handle creq.handle)
-					 (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_MAP_DUMB &mreq)))
-					 (let ((map :type uint32_t*
-						    :init (funcall static_cast<uint32_t*> (funcall mmap 0 creq.size
-								    (\| PROT_READ PROT_WRITE)
-								    MAP_SHARED fd mreq.offset))))
-					   (funcall assert (!= MAP_FAILED map))
-					    (funcall memset map 0 creq.size)
+			    (let ((fd :init (paren-list (let ((fd :init (funcall drmOpen (string "i915") nullptr)))
+							  (if (< fd 0)
+							      (macroexpand (er "drmOpen error: " fd)))
+							  (let ((rr :init (funcall drmSetClientCap fd DRM_CLIENT_CAP_UNIVERSAL_PLANES 1)))
+							    (if (!= 0 rr)
+								(macroexpand (er "drmSetClientCap error: " rr))))
+							  (raw "fd"))))
+				  (res :init  (paren-list (let ((r :init (funcall drmModeGetResources fd)))
+							    (funcall assert r)
+							    (raw "r"))))
+				  (c :type drmModeConnectorPtr
+				     :init (paren-list
+					    (let ((c :type drmModeConnectorPtr :init nullptr))
+					      (dotimes (i (funcall "static_cast<unsigned int>" res->count_connectors))
+						(setf c  (funcall drmModeGetConnector fd (aref res->connectors i)))
+						(funcall assert c)
+						(if (== DRM_MODE_CONNECTED
+							c->connection)
+						    (break))
+						(funcall drmFree c))
+					      (raw "c")))))
+			      
+			      (let ((enc :init (funcall drmModeGetEncoder fd c->encoder_id))
+				    (crtc :init (funcall drmModeGetCrtc fd enc->crtc_id))
+				    (fb :init (funcall drmModeGetFB fd crtc->buffer_id))
+				    (plane_res :init (funcall drmModeGetPlaneResources fd))
+				    (plane  :init (paren-list
+						   (let ((p :type drmModePlanePtr :init nullptr))
+						     (dotimes (i plane_res->count_planes)
+						       (setf p (funcall drmModeGetPlane fd (aref plane_res->planes i)))
+						       (funcall assert p)
+						       (if (== p->fb_id fb->fb_id)
+							   (break))
+						       (funcall drmFree p))
+						     (raw "p")))))
+				(statements
+				 ,@(loop for e in '(enc crtc fb plane_res) collect
+					`(funcall assert ,e)))
+				
+				(let ((has_dumb :type uint64_t :init 0))
+				  (funcall assert (! (funcall drmGetCap fd DRM_CAP_DUMB_BUFFER &has_dumb)))
+				  (funcall assert has_dumb))
+				(let ((creq :type "struct drm_mode_create_dumb"))
+				  (funcall memset &creq 0 (funcall sizeof creq))
+				  (setf creq.width fb->width
+					creq.height fb->height
+					creq.bpp fb->bpp)
+				  (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_CREATE_DUMB &creq)))
+				  (macroexpand (e "width=" creq.width " height=" creq.height))
+				  (let ((my_fb :type uint32_t :init 0))
+				    (funcall assert
+					     (! (funcall drmModeAddFB fd creq.width creq.height 24
+							 creq.bpp creq.pitch creq.handle &my_fb)))
+				    (let ((mreq :type "struct drm_mode_map_dumb"))
+				      (funcall memset &mreq 0 (funcall sizeof mreq))
+				      (setf mreq.handle creq.handle)
+				      (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_MAP_DUMB &mreq)))
+				      (let ((map :type uint32_t*
+						 :init (funcall static_cast<uint32_t*> (funcall mmap 0 creq.size
+												(\| PROT_READ PROT_WRITE)
+												MAP_SHARED fd mreq.offset))))
+					(funcall assert (!= MAP_FAILED map))
+					(funcall memset map 0 creq.size)
 
-					   (dotimes (i 256)
-					     (dotimes (j 256)
-					       (setf (aref map (+ j (* i (>> creq.pitch 2))))
-						     (hex #x12345678))))
-					   (funcall assert (! (funcall drmModeSetCrtc fd crtc->crtc_id my_fb
-								       0 0 &c->connector_id 1 &crtc->mode)))
-					   (funcall sleep 10)
-					   (funcall assert (! (funcall drmModeSetCrtc fd crtc->crtc_id fb->fb_id
-								       0 0 &c->connector_id 1 &crtc->mode)))
-					   (funcall assert (! (funcall drmModeRmFB fd my_fb)))
-					   (let ((dreq :type "struct drm_mode_destroy_dumb"))
-					     (funcall memset &dreq 0 (funcall sizeof dreq))
-					     (setf dreq.handle creq.handle)
-					     (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_DESTROY_DUMB &dreq))))
-					   (statements
-					    ,@(loop for e in '(plane plane_res fb crtc enc c res) collect
-						   `(funcall drmFree ,e)))
-					   (funcall drmClose fd)))))))))
+					(dotimes (i 256)
+					  (dotimes (j 256)
+					    (setf (aref map (+ j (* i (>> creq.pitch 2))))
+						  (hex #x12345678))))
+					(funcall assert (! (funcall drmModeSetCrtc fd crtc->crtc_id my_fb
+								    0 0 &c->connector_id 1 &crtc->mode)))
+					(funcall sleep 10)
+					(funcall assert (! (funcall drmModeSetCrtc fd crtc->crtc_id fb->fb_id
+								    0 0 &c->connector_id 1 &crtc->mode)))
+					(funcall assert (! (funcall drmModeRmFB fd my_fb)))
+					(let ((dreq :type "struct drm_mode_destroy_dumb"))
+					  (funcall memset &dreq 0 (funcall sizeof dreq))
+					  (setf dreq.handle creq.handle)
+					  (funcall assert (! (funcall drmIoctl fd DRM_IOCTL_MODE_DESTROY_DUMB &dreq))))
+					(statements
+					 ,@(loop for e in '(plane plane_res fb crtc enc c res) collect
+						`(funcall drmFree ,e)))
+					(funcall drmClose fd))))))
+)
 			    
 			    
 			    
